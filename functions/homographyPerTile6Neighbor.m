52abf12d87832bb18b3fad42f9bd0f46131567b9
Erhan Bas <erhanbas@gmail.com>
2018-12-05 15:00:48 -0500

    minor cleaning

--- functions/homographyPerTile6Neighbor.m
+++ functions/homographyPerTile6Neighbor.m
@@ -1,4 +1,4 @@
-function [scopeparams,scopeparams_,paireddescriptor,curvemodel] = ...
+function [scopeparams,scopeparamsUpdated,paireddescriptor,curvemodel] = ...
     homographyPerTile6Neighbor(params,neighbors,scopeloc,paireddescriptor,curvemodel,unreliable,neigbors_used)
 %HOMOGRAPHYPERTILE Summary of this function goes here
 % 
@@ -19,15 +19,15 @@ function [scopeparams,scopeparams_,paireddescriptor,curvemodel] = ...
 checkthese = [1 4 5 7]; % 0 - right - bottom - below
 imsize_um = params.imsize_um;
 neigs = neighbors(:,checkthese);%[id -x -y +x +y -z +z] format
-Nneig = size(neigs,1);
-stgdisp = NaN(Nneig,3);
-for ineig = 1:Nneig
-    idxcent = neigs(ineig,1);
+Ntiles = size(neigs,1);
+stgdisp = NaN(Ntiles,3);
+for itile = 1:Ntiles
+    idxcent = neigs(itile,1);
     for iadj = 1:3
-        idxadj = neigs(ineig,1+iadj);
+        idxadj = neigs(itile,1+iadj);
         if isnan(idxadj)
         else
-            stgdisp(ineig,iadj) = 1000*(scopeloc.loc(idxadj,iadj)-scopeloc.loc(idxcent,iadj));
+            stgdisp(itile,iadj) = 1000*(scopeloc.loc(idxadj,iadj)-scopeloc.loc(idxcent,iadj));
         end
     end
 end
@@ -44,46 +44,37 @@ params.order = 1;
 [scopeparams,validthis] = util.estimateaffine(paireddescriptor,neighbors,scopeloc,params,curvemodel,0);
 
 %% affine outlier: TODO, reject based on tile corner pixel shift magnitudes!!
-
 %mean transformation
-if 1
-    aff = mean(reshape([scopeparams(:).affineglFC],3,3,[]),3);
-else
-    afsum = zeros(3);
-    iter=0;
-    for ineig = 1:Nneig
-        af = scopeparams(ineig).affineglFC;
-        if isempty(af)
-        else
-            afsum = afsum+af;
-            iter=iter+1;
-        end
-    end
-    aff = afsum/iter;
-end
-%%
-scopeparams_ = scopeparams;
-reliable = zeros(Nneig,1);
-for ineig = 1:Nneig
-    if isempty(scopeparams_(ineig).affineglFC) | norm(scopeparams_(ineig).affineglFC-aff)/norm(aff)*100>1
-        %not reliable
-    else
-        reliable(ineig) = 1;
-    end
-end
+aff = mean(reshape([scopeparams(:).affineglFC],3,3,[]),3);
 
+scopeparamsUpdated = scopeparams;
+reliable = nan(Ntiles,1);
+for itile = 1:Ntiles
+    if isempty(scopeparamsUpdated(itile).affineglFC) 
+        reliable(itile) = 0;
+        disp(sprintf('WARNING: Bad affine estimate @tile: %d [%4.2f %4.2f %4.2f]',itile,(1000*scopeloc.loc(itile,:)))) %#ok<*DSPS>
+    elseif norm(scopeparamsUpdated(ineig).affineglFC-aff)/norm(aff)*100>1
+        disp(sprintf('WARNING: Bad affine estimate @tile: %d [%4.2f %4.2f %4.2f]',itile,(1000*scopeloc.loc(itile,:))))
+        reliable(itile) = 0;
+    else
+        reliable(itile) = 1;
+    end
+end
+if sum(~reliable);return;end
+%%
 inliers = find(reliable(:));
 % for every tiles estimate an affine
 anchors = scopeloc.gridix(inliers,1:3);
 queries = scopeloc.gridix(:,1:3);
 IDX = knnsearch(anchors,queries,'K',1,'distance',@distfun);%W=[1 1 100000]
 % fill missing 
-for ineig = 1:Nneig
-    ianch = inliers(IDX(ineig));
-    paireddescriptor{ineig}.onx = paireddescriptor{ianch}.onx;
-    paireddescriptor{ineig}.ony = paireddescriptor{ianch}.ony;
-    curvemodel(:,:,ineig) = curvemodel(:,:,ianch);
-    scopeparams_(ineig) = scopeparams(ianch);
+for itile = 1:Ntiles
+    ianch = inliers(IDX(itile));
+    if itile == ianch; continue;end % skip if ancher is tile itself
+    paireddescriptor{itile}.onx = paireddescriptor{ianch}.onx;
+    paireddescriptor{itile}.ony = paireddescriptor{ianch}.ony;
+    curvemodel(:,:,itile) = curvemodel(:,:,ianch);
+    scopeparamsUpdated(itile) = scopeparams(ianch);
 end
 
 
