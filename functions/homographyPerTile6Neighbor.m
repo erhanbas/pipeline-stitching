d6edc78d6a6056b747cd4f88acb0cc00ac82fb11
Erhan Bas <erhanbas@gmail.com>
2018-12-05 14:08:18 -0500

    cleaned up the code, added efficient mean calculation

--- functions/homographyPerTile6Neighbor.m
+++ functions/homographyPerTile6Neighbor.m
@@ -1,5 +1,5 @@
 function [scopeparams,scopeparams_,paireddescriptor,curvemodel] = ...
-    homographyPerTile6Neighbor(params,neighbors,scopeloc,paireddescriptor,R,curvemodel)
+    homographyPerTile6Neighbor(params,neighbors,scopeloc,paireddescriptor,curvemodel,unreliable,neigbors_used)
 %HOMOGRAPHYPERTILE Summary of this function goes here
 % 
 % [OUTPUTARGS] = HOMOGRAPHYPERTILE(INPUTARGS) Explain usage here
@@ -38,69 +38,29 @@ if 0 % based on overall displacement
 else %based on per tile
     xyz_umperpix_model = abs(stgdisp'./squeeze(curvemodel(:,3,:)));
 end
-foundmatch = all(isfinite(xyz_umperpix_model(1:2,:)));
-xyz_umperpix_model(1:2,foundmatch);
-
-
-%% OUTLIER DETECTION
-if 0
-    %% based on median 
-    % TODO: no need for this anymore as we do this in
-    % curvatureOutlierElimination function !!
-    validtiles=squeeze(all(curvemodel(1:2,1,:)|curvemodel(1:2,3,:)));
-    [thrs,medcurvemodel] = estimateFCthreshols(curvemodel(:,:,validtiles),.99);
-    medcurvemodelcents = medcurvemodel(1:2,[1 3]);
-    thrcurvemodelcents = thrs(1:2,[1 3]);
-    count = zeros(Nneig,2);
-    for ineig = 1:Nneig
-        [count(ineig,:)] = paireddescriptor{ineig}.count;
-    end
-    unreliable = zeros(Nneig,1);
-    for ineig = 1:Nneig
-        fccent = curvemodel(1:2,[1 3],ineig);
-        if any(abs(fccent(:)-medcurvemodelcents(:))>thrcurvemodelcents(:))
-            unreliable(ineig) = 1;
-        end
-    end
-    %%
-    inliers = find(~unreliable);
-    % for every tiles estimate an affine
-    anchors = scopeloc.gridix(inliers,1:3);
-    queries = scopeloc.gridix(:,1:3);
-    IDX = knnsearch(anchors,queries,'K',1,'distance',@distfun);%W=[1 1 100000]
-    
-    %%
-    % fill missing
-    for ineig = 1:Nneig
-        ianch = inliers(IDX(ineig));
-        paireddescriptor{ineig}.onx = paireddescriptor{ianch}.onx;
-        paireddescriptor{ineig}.ony = paireddescriptor{ianch}.ony;
-        paireddescriptor{ineig}.count = [size(paireddescriptor{ineig}.onx.X,1) size(paireddescriptor{ineig}.ony.X,1)];
-        curvemodel(:,:,ineig) = curvemodel(:,:,ianch);
-    end
-else
-    validtiles=squeeze(all(curvemodel(1:2,1,:)|curvemodel(1:2,3,:)));
-    % rejection based on projected points
-end
 
 %%
 params.order = 1;
 [scopeparams,validthis] = util.estimateaffine(paireddescriptor,neighbors,scopeloc,params,curvemodel,0);
 
 %% affine outlier: TODO, reject based on tile corner pixel shift magnitudes!!
-%mean transformation
-afsum = zeros(3);
-iter=0;
-for ineig = 1:Nneig
-    af = scopeparams(ineig).affineglFC;
-    if isempty(af)
-    else
-       afsum = afsum+af;
-       iter=iter+1;
-    end
-end
-aff = afsum/iter;
 
+%mean transformation
+if 1
+    aff = mean(reshape([scopeparams(:).affineglFC],3,3,[]),3);
+else
+    afsum = zeros(3);
+    iter=0;
+    for ineig = 1:Nneig
+        af = scopeparams(ineig).affineglFC;
+        if isempty(af)
+        else
+            afsum = afsum+af;
+            iter=iter+1;
+        end
+    end
+    aff = afsum/iter;
+end
 %%
 scopeparams_ = scopeparams;
 reliable = zeros(Nneig,1);
